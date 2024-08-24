15a3522099f56ba1d5ac501908352f08cd2a8cd5
Sumair Aziz <140709914+SumairAziz@users.noreply.github.com>
2024-08-24 11:12:28 +0500

    third commit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: '/myPortfolio/',
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/myPortfolio/' : '/',
   plugins: [react()],
 })
