f6c7d2f987bf9125e39e7b70294a9df0510c2756
Sumair Aziz <140709914+SumairAziz@users.noreply.github.com>
2024-08-26 16:04:03 +0500

    final commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 opacity-80 bg-primary brightness-125`}>
       <div className='w-full flex justify-between items-center mx-auto'>
         <Link
-          to="\"
+          to="/"
           className='flex items-center gap-2'
           onClick={() => {
             setActive("")
